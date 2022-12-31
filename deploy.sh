818d2148532a0b24bf7a55db62c4e01007dee98e
Tin Nguyen <hopthucuatin@gmail.com>
2022-12-31 19:38:04 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 
 echo ">> Deploying contract"
 
-near dev-deploy --wasmFile ./target/wasm32-unknown-unknown/release/nft_marketplace.wasm
+near dev-deploy --wasmFile ../target/wasm32-unknown-unknown/release/fungible_token.wasm --initFunction new_default_meta --initArgs '{"owner_id": "trungtin2001.testnet", "total_supply": "100000000000000000000000000000"}'  -f
